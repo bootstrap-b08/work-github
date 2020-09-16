007c885f16a0c45ce71439576a606d0e1bbb29d6
vagrant <vagrant@localhost.localdomain>
2020-09-16 09:52:24 +0000

    [U[Add] activeadminの追加

--- db/migrate/20200916032120_rename_is_active_colum_to_genres.rb
+++
@@ -1,5 +0,0 @@
-class RenameIsActiveColumToGenres < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :genres, :IsActive, :is_active
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_032120) do
+ActiveRecord::Schema.define(version: 2020_09_16_093852) do
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.integer "resource_id"
+    t.string "author_type"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -21,16 +35,16 @@ ActiveRecord::Schema.define(version: 2020_09_16_032120) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email"
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "cart_items", force: :cascade do |t|
@@ -71,19 +85,19 @@ ActiveRecord::Schema.define(version: 2020_09_16_032120) do
   create_table "items", force: :cascade do |t|
     t.integer "genre_id"
     t.string "name"
-    t.text "introduction"
-    t.boolean "is_active"
     t.string "image_id"
+    t.text "introduction"
     t.integer "price"
+    t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "order_items", force: :cascade do |t|
-    t.integer "item_id"
     t.integer "order_id"
-    t.integer "quantity"
+    t.integer "item_id"
     t.integer "order_price"
+    t.integer "quantity"
     t.integer "product_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
