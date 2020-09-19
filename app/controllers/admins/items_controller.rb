37f5712bc7e5c3bd9b550c5e7564005ea46d204a
vagrant <vagrant@localhost.localdomain>
2020-09-19 13:09:57 +0000

    [Add] admin_items controller仮追加

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,2 +1,42 @@
 class Admins::ItemsController < ApplicationController
+
+	def new
+		@item = Item.new
+	end
+
+	def create
+		@item = Item.new(item_params)
+		if @item.save
+			redirect_to admins_items_path
+		else
+			render :new
+		end
+	end
+
+	def index
+		@items = Item.all
+	end
+
+	def show
+		@item = Item.find(params[:id])
+	end
+
+	def edit
+		@item = Item.find(params[:id])
+	end
+
+	def update
+		@item = Item.find(params[:id])
+		if @item.update(item_params)
+			redirect_to admins_item_path(@item.id)
+		else
+			render :edit
+		end
+	end
+
+	private
+	def item_params
+		params.require(:item).permit(:name, :image_id, :introduction, :price, :is_active)
+	end
+
 end
