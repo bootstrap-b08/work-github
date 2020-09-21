350d6536f307d58ba15f56cda25df80f55cc4f4e
vagrant <vagrant@localhost.localdomain>
2020-09-21 03:17:43 +0000

    [add]second

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -1,2 +1,49 @@
-class Customers::CustomersController < ApplicationController
+class CustomersController < ApplicationController
+      before_action :ensure_correct_customer, {only: [:show, :edit]}
+
+
+  def show
+  	@customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:success] = "You have edited user data successfully."
+      redirect_to customer_path(@customer)
+    else
+      render 'edit'
+    end
+  end
+
+    def unsubscribe #退会画面を表示するアクション
+    end
+
+    def withdrow
+       @customer = Customer.find(params[:id])
+       if @customer.update(is_deleted: true)
+          sign_out current_customer #URLを踏ませずにコントローラーから直接サインアウトの指示を出す（device公式)
+       end
+       redirect_to root_path
+    end
+
+
+
+  private
+  def customer_params
+  	  params.require(:customer).permit(:is_deleted, :family_name, :first_name, :family_name_kana, :first_name_kana,
+  	                                   :telephone_number, :email, :password, :postel_code, :address)
+  end
+
+  def ensure_correct_customer
+    @customer = Customer.find(params[:id])
+    if current_customer.id != @customer.id
+       redirect_to root_path
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# CustomerModel(2)
+
+Customer.create!(
+                  is_deleted: "false",
+                  family_name: "山田",
+                  first_name: "花子1号",
+                  family_name_kana: "ヤマダ",
+                  first_name_kana: "ハナコ",
+                  telephone_number: "00000000000",
+                  email: "hoge1@mail",
+                  password: "hogehoge",
+
+                  postel_code: "0000000",
+
+                  address: "東京都 渋谷区",
+                  )
+Customer.create!(
+                  is_deleted: "false",
+                  family_name: "山田",
+                  first_name: "花子2号",
+                  family_name_kana: "ヤマダ",
+                  first_name_kana: "ハナコ",
+                  telephone_number: "11111111111",
+                  email: "hoge2@mail",
+                  password: "hogehoge",
+
+                  postel_code: "1111111",
+
+                  address: "東京都 渋谷区",
+                  )
