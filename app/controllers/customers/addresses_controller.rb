4075001c4189c47cd8c847f2fd24ee3d52b59f71
vagrant <vagrant@localhost.localdomain>
2020-09-22 09:56:49 +0000

    [update]addressController

--- app/controllers/customers/addresses_controller.rb
+++ app/controllers/customers/addresses_controller.rb
@@ -7,12 +7,12 @@ class Customers::AddressesController < ApplicationController
   def create
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
-    if @address.save
-       redirect_to addresses_path
+    if  @address.save
+        redirect_to addresses_path
     else
-       @address_new = Address.new
-       @addresses = current_customer.addresses
-       render :index
+        @address_new = Address.new
+        @addresses = current_customer.addresses
+        render :index
     end
   end
 
@@ -22,8 +22,12 @@ class Customers::AddressesController < ApplicationController
 
   def update
     address = Address.find(params[:id])
-    address.update(address_params)
-    redirect_to addresses_path
+    if  address.update(address_params)
+        redirect_to addresses_path
+    else
+        @address = Address.find(params[:id])
+        render :edit
+    end
   end
 
   def destroy
