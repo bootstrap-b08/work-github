9e6a89cff07858e2659101906a7ac56becd13420
vagrant <vagrant@localhost.localdomain>
2020-09-28 13:41:26 +0000

    最çadmin add link

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -3,8 +3,14 @@ class Admins::OrdersController < ApplicationController
 	before_action :set_order, except: [:index]
 
 	def index
-		@orders = Order.page(params[:page]).per(10)
-	end
+      	if params[:id] # 会員詳細から来た場合
+      		@orders = Customer.find(params[:id]).orders.page(params[:page]).per(10)
+		elsif request.fullpath.include? "today" # TOP本日受注分から来た場合
+      		@orders = Order.where(created_at:  Time.zone.now.all_day).page(params[:page]).per(10)
+    	else
+     		@orders = Order.page(params[:page]).per(10)
+    	end
+  	end
 
 	def show
 	end
@@ -36,7 +42,7 @@ class Admins::OrdersController < ApplicationController
 	end
 
 	def order_params
-	  params.require(:order).permit(:shipping_addres, :postal_code, :delivery_name, :shipping_cost, :billing_amount, :payment_method, :order_status)
+	  params.require(:order).permit(:customer_id, :shipping_addres, :postal_code, :delivery_name, :shipping_cost, :billing_amount, :payment_method, :order_status)
 	end
 
 	def product_status_params
@@ -46,5 +52,5 @@ class Admins::OrdersController < ApplicationController
 	def item_params
 	  params.require(:item).permit(:price)
 	end
-  
+
 end
